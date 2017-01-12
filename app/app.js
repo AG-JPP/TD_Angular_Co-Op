71e4b705aabfd86d9220996205d109efa58116d2
giovanne3u <giovannelli.alexis@gmail.com>
2017-01-12 10:15:13 +0100

    key

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 var app = angular.module("coop", ["ngResource"]);
-app.constant('api', {'key' : '', 'url' : 'http://coop.api.netlor.fr'});
+app.constant('api', {'key' : '4cfd432d26a045708e852568197c7956', 'url' : 'http://coop.api.netlor.fr'});
 
 app.config(['$httpProvider', "api", function($httpProvider, api){
   $httpProvider.defaults.headers.commom.Authorization = 'Token token' = api.key;
