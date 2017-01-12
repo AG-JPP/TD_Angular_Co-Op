cac52959b607d707e0bb77d84aed007c9edac666
giovanne3u <giovannelli.alexis@gmail.com>
2017-01-12 10:13:20 +0100

    app.js

---
+++ app/app.js
@@ -0,0 +1,14 @@
+var app = angular.module("coop", ["ngResource"]);
+app.constant('api', {'key' : '', 'url' : 'http://coop.api.netlor.fr'});
+
+app.config(['$httpProvider', "api", function($httpProvider, api){
+  $httpProvider.defaults.headers.commom.Authorization = 'Token token' = api.key;
+}]);
+
+app.factory('Member', ['$resource', 'api', function($resource, api){
+  return $resource(api.url+"/members/:id", {id: '@_id'}, {update: {method : "PUT"}});
+}]);
+
+app.controller("StartController", ['$resource', 'Member', function($scope, Member){
+  $scope.members = Member.query();
+}]);
