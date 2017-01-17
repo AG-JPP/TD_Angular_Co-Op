5552fb86e751c3eece8ebcdda69caf45d1f8b266
pereirad9u <alexandre.pereira.da@gmail.com>
2017-01-17 17:42:15 +0100

    ajout verif' token

--- app/app.js
+++ app/app.js
@@ -1,22 +1,22 @@
 var app = angular.module("coop", ["ngResource", 'ngRoute']);
 app.constant('api', {'key': '4cfd432d26a045708e852568197c7956', 'url': 'http://coop.api.netlor.fr/api'});
 
-app.config(['$httpProvider', "api", 'TokenServiceProvider',  function ($httpProvider, api, TokenServiceProvider) {
+app.config(['$httpProvider', "api", 'TokenServiceProvider', function ($httpProvider, api, TokenServiceProvider) {
     $httpProvider.defaults.headers.common.Authorization = 'Token token=' + api.key;
 
-    $httpProvider.interceptors.push(['TokenService', function(TokenService) {
+    $httpProvider.interceptors.push(['TokenService', function (TokenService) {
         return {
-            request: function(config){
+            request: function (config) {
                 var token = TokenService.getToken();
-                if (token != "" )
-                    config.url += ((config.url.indexOf('?') >=0) ? '&' : '?') + 'token=' + token;
+                if (token != "")
+                    config.url += ((config.url.indexOf('?') >= 0) ? '&' : '?') + 'token=' + token;
                 return config;
             }
         }
     }])
 }]);
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'templates/login.html',
         controller: 'StartController'
@@ -38,27 +38,56 @@ app.config(['$routeProvider', function($routeProvider) {
     });
 }]);
 
+app.run(['$rootScope', '$location', 'Member', 'TokenService', function ($rootScope, $location, Member, TokenService) {
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        if (localStorage.getItem('token') != null) {
+            TokenService.setToken(localStorage.getItem('token'));
+            Member.signedin({id: localStorage.getItem('id')}, function (m) {
+            }, function (error) {
+                TokenService.setToken("");
+                localStorage.clear();
+                $location.path("/");
+            });
+        } else {
+            if (localStorage.getItem('token') == null) {
+                $location.path("/")
+            } else {
+                TokenService.setToken(localStorage.getItem('token'));
+                Member.signedin({id: localStorage.getItem('id')}, function (m) {
+                }, function (error) {
+                    TokenService.setToken("");
+                    localStorage.clear();
+                    $location.path("/");
+                });
+            }
+        }
+    });
+}]);
+
+
+
 app.factory('Member', ['$resource', 'api', function ($resource, api) {
     return $resource(api.url + "/members/:id", {id: '@_id'},
         {
             update: {method: "PUT"},
-            signin: {method: "POST", url: api.url + "/members/signin"}
+            signin: {method: "POST", url: api.url + "/members/signin"},
+            signedin: {method : 'GET', url:api.url+'/members/:id/signedin'}
         });
 }]);
 
-app.factory('Channels', ['$resource', 'api', function($resource, api){
-  return $resource(api.url + "/channels/:id", {id: "@_id"},
-      {
-        delete : {method : "DELETE", url : api.url + "/channels/:id"}
-      });
+app.factory('Channels', ['$resource', 'api', function ($resource, api) {
+    return $resource(api.url + "/channels/:id", {id: "@_id"},
+        {
+            delete: {method: "DELETE", url: api.url + "/channels/:id"}
+        });
 }]);
 
-app.service('TokenService', [function() {
+app.service('TokenService', [function () {
     this.token = "";
-    this.setToken = function(t) {
+    this.setToken = function (t) {
         this.token = t;
     }
-    this.getToken = function() {
+    this.getToken = function () {
         return this.token;
     }
 }]);
@@ -74,9 +103,9 @@ app.controller("StartController", ['$resource', "$scope", "$location", 'Member',
         }
     );
 
-    $scope.redirectRegister = function(){
-      $location.path('/register');
-      $location.replace();
+    $scope.redirectRegister = function () {
+        $location.path('/register');
+        $location.replace();
     }
 
     // $scope.ajoutMembre = function(){
@@ -94,66 +123,68 @@ app.controller("StartController", ['$resource', "$scope", "$location", 'Member',
     //   );
     // }
 
-    $scope.loginMembre = function(){
-      $scope.member = Member.signin({
-        email : $scope.member.email,
-        password : $scope.member.pass,
-      },
-      function (m) {
-          $scope.member = m;
-          console.log($scope.member);
-          TokenService.setToken($scope.member.token);
-          localStorage.setItem("token", TokenService.getToken());
-          localStorage.setItem("id", $scope.member._id);
-          $scope.member = Member.query(function(member) {});
-          $location.path('/channels');
-          $location.replace();
-      },
-      function (e) {
-          console.log(e);
-      }
-  )}
+    $scope.loginMembre = function () {
+        $scope.member = Member.signin({
+                email: $scope.member.email,
+                password: $scope.member.pass,
+            },
+            function (m) {
+                $scope.member = m;
+                console.log($scope.member);
+                TokenService.setToken($scope.member.token);
+                localStorage.setItem("token", TokenService.getToken());
+                localStorage.setItem("id", $scope.member._id);
+                $scope.member = Member.query(function (member) {
+                });
+                $location.path('/channels');
+                $location.replace();
+            },
+            function (e) {
+                console.log(e);
+            }
+        )
+    }
 
 }]);
 
-app.controller('registerController', ['$resource', '$scope', 'Member', function($resource, $scope, Member){
+app.controller('registerController', ['$resource', '$scope', 'Member', function ($resource, $scope, Member) {
 
-  $scope.ajoutMembre = function(){
-    $scope.newMember = new Member({
-        fullname: $scope.member.fullname,
-        email:  $scope.member.email,
-        password: $scope.member.pass,
-    });
-    $scope.newMember.$save(function(success){
-      console.log(success);
-     },
-       function(error){
-       console.log(error);
-     }
-    );
-  }
+    $scope.ajoutMembre = function () {
+        $scope.newMember = new Member({
+            fullname: $scope.member.fullname,
+            email: $scope.member.email,
+            password: $scope.member.pass,
+        });
+        $scope.newMember.$save(function (success) {
+                console.log(success);
+            },
+            function (error) {
+                console.log(error);
+            }
+        );
+    }
 
 }]);
 
-app.controller('channelsController', ['$resource', '$scope', 'Channels', function($resource, $scope, Channels){
-  $scope.channels = Channels.query(
-    function(success){
+app.controller('channelsController', ['$resource', '$scope', 'Channels', function ($resource, $scope, Channels) {
+    $scope.channels = Channels.query(
+        function (success) {
 
-    },
-    function(error){
-      console.log(error);
-    });
+        },
+        function (error) {
+            console.log(error);
+        });
 
-    $scope.createChannel = function(){
-      $scope.newChannel = new Channels({
-        label: $scope.channel.label,
-        topic: $scope.channel.topic,
-      });
-      $scope.newChannel.$save(function(success){
+    $scope.createChannel = function () {
+        $scope.newChannel = new Channels({
+            label: $scope.channel.label,
+            topic: $scope.channel.topic,
+        });
+        $scope.newChannel.$save(function (success) {
 
-      },
-      function(error){
-        console.log(error);
-      });
+            },
+            function (error) {
+                console.log(error);
+            });
     }
 }]);
